7f088f8db3c043bd50e3c0603d6cf3f4d30b81b1
ajenee1 <alexanderjenee@live.com>
2025-09-28 22:21:16 +0200

    Code done

--- Bayes Classifiers and Boosting/lab3py/labfuns.py
+++ Bayes Classifiers and Boosting/lab3py/labfuns.py
@@ -287,7 +287,7 @@ class DecisionTreeClassifier(object):
 
     def trainClassifier(self, Xtr, yTr, W=None):
         rtn = DecisionTreeClassifier()
-        rtn.classifier = tree.DecisionTreeClassifier(max_depth=Xtr.shape[1]/2+1)
+        rtn.classifier = tree.DecisionTreeClassifier(max_depth=int(Xtr.shape[1]/2+1))
         if W is None:
             rtn.classifier.fit(Xtr, yTr)
         else:
